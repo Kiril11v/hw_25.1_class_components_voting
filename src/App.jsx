fa336a6f3b5ee6d7e5e3b876ab6d48060c89804d
Kyrylo_Kud <kudkiril@gmail.com>
2025-09-16 17:24:19 +0200

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ class App extends Component {
     super(props);
     this.images = [loving, crying, smiling, sleeping, withSunGlasses];
 
-    // Инициализация state с LocalStorage
     let savedVotes;
     try {
       const stored = localStorage.getItem('votes');
@@ -65,44 +64,60 @@ class App extends Component {
     const { votes, winnersEmoji } = this.state;
 
     return (
-      <div>
-        <h1 className='fw-bold'>Голосування за найкращий смайлик</h1>
-        <div className='d-flex gap-5 justify-content-between my-5 flex-wrap align-center'>
+      <div className="container py-4">
+        <h1 className="fw-bold text-center mb-4">
+          Голосування за найкращий смайлик
+        </h1>
+
+        {/* эмодзи в сетке */}
+        <div className="row g-4 justify-content-center">
           {this.images.map((src, index) => (
-            <Counter
-              key={index}
-              src={src}
-              alt={`emoji-${index}`}
-              votes={votes[index]}
-              onVote={() => this.handleVote(index)}
-            />
+            <div key={index} className="col-6 col-sm-4 col-md-3 col-lg-2 text-center">
+              <Counter
+                src={src}
+                alt={`emoji-${index}`}
+                votes={votes[index]}
+                onVote={() => this.handleVote(index)}
+              />
+            </div>
           ))}
         </div>
 
-        <button onClick={this.showResults}>Show Results</button>
+        <div className="text-center mt-4">
+          <button onClick={this.showResults}>
+            Show Results
+          </button>
+        </div>
 
         {winnersEmoji.length > 0 && (
-          <div>
-            <p className='fw-bold fs-2'>Результати голосування:</p>
+          <div className="mt-5 text-center">
+            <p className="fw-bold fs-2">Результати голосування:</p>
             {winnersEmoji.length > 1 ? (
               <p className="fw-bold fs-4">Нічия:</p>
             ) : (
-              <p className='fw-bold fs-4'>Переможець:</p>
+              <p className="fw-bold fs-4">Переможець:</p>
             )}
-            <div className='d-flex flex-wrap gap-3 justify-content-center'>
+
+            <div className="row g-3 justify-content-center">
               {winnersEmoji.map(i => (
-                <div key={i} className="text-center">
-                  <img 
+                <div key={i} className="col-6 col-sm-4 col-md-3 col-lg-2">
+                  <img
                     src={this.images[i]}
                     alt={`winner-${i}`}
                     style={{ width: '120px' }}
-                    className="emoji-winner"
+                    className="emoji-winner img-fluid"
                   />
                 </div>
               ))}
             </div>
-            <p className="fw-bold fs-5">Кількість голосів: {Math.max(...votes)}</p>
-            <button onClick={this.resetResults}>Очистити результати</button>
+
+            <p className="fw-bold fs-5 mt-3">
+              Кількість голосів: {Math.max(...votes)}
+            </p>
+
+            <button onClick={this.resetResults}>
+              Очистити результати
+            </button>
           </div>
         )}
       </div>
